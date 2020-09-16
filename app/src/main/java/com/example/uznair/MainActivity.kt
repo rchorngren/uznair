98ba8ba04451c95f68c17917e32f02caa7ec4c69
Robert Horngren <robert@horngren.net>
2020-09-16 13:53:44 +0200

    Minor adjustment to styling of start screen Added input of player name and logic to only press play button after a name has been entered

--- app/src/main/java/com/example/uznair/MainActivity.kt
+++ app/src/main/java/com/example/uznair/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.uznair
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var playButton: Button
+    lateinit var playerNameInputField :  TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playButton = findViewById(R.id.playButton)
+        playButton.isEnabled = false
+
+        playerNameInputField = findViewById(R.id.editTextTextPersonName)
+
+        playerNameInputField.addTextChangedListener(object: TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                playButton.isEnabled = s.toString().trim{ it <= ' '}.isNotEmpty()
+            }
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+            override fun afterTextChanged(s: Editable) {
+            }
+
+        })
     }
+
+
+
+
+
+    fun playButton(view: View) {
+        var playerName = findViewById<TextView>(R.id.editTextTextPersonName).text.toString()
+        Log.d("!!!", "playerName: $playerName")
+    }
+
+
 }
