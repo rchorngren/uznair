6fca72173c79d3f42a80d4495e9bf46d8a64b87e
mirandamutka <miranda.mutka@outlook.com>
2020-10-01 10:42:05 +0200

    Added initial flip animation for CorrectFragment.kt and IncorrectFragment.kt

--- app/src/main/java/com/example/uznair/CorrectFragment.kt
+++ app/src/main/java/com/example/uznair/CorrectFragment.kt
@@ -1,5 +1,7 @@
 package com.example.uznair
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.media.Image
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -14,6 +16,10 @@ import kotlinx.android.synthetic.main.fragment_correct.*
 class CorrectFragment : Fragment() {
 
     lateinit var correctCardImage : ImageView
+    lateinit var backCardImage : ImageView
+    lateinit var front_anim : AnimatorSet
+    lateinit var back_anim : AnimatorSet
+    var isFront : Boolean = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,18 +28,19 @@ class CorrectFragment : Fragment() {
     ): View? {
 
         val view = inflater.inflate(R.layout.fragment_correct, container, false)
+
+        correctCardImage = view.findViewById(R.id.correctFragmentCard)
+        backCardImage = view.findViewById(R.id.backCorrectFragmentCard)
+        var cardNumber = (activity as GameActivity).newRandomNumber
+
+        showCard(cardNumber)
+
         var closeCorrectFragmentButton = view.findViewById<Button>(R.id.closeCorrectFragmentButton)
 
         closeCorrectFragmentButton.setOnClickListener {
             (activity as GameActivity).removeCorrectFragment()
             (activity as GameActivity).anotherGame()
         }
-
-        correctCardImage = view.findViewById(R.id.correctFragmentCard)
-        var cardNumber = (activity as GameActivity).newRandomNumber
-
-        showCard(cardNumber)
-
         return view
     }
 
@@ -69,6 +76,27 @@ class CorrectFragment : Fragment() {
             10 -> {
                 correctCardImage.setImageResource(R.drawable.card10)
             }
+        }
+        val context = (activity as GameActivity).applicationContext
+        val scale = context.resources.displayMetrics.density
+        correctCardImage.cameraDistance = 8000 * scale
+        backCardImage.cameraDistance = 8000 * scale
+
+        front_anim = AnimatorInflater.loadAnimator(context, R.animator.front_animator) as AnimatorSet
+        back_anim = AnimatorInflater.loadAnimator(context, R.animator.back_animator) as AnimatorSet
+
+        isFront = if(isFront){
+            front_anim.setTarget(backCardImage)
+            back_anim.setTarget(correctCardImage)
+            front_anim.start()
+            back_anim.start()
+            false
+        } else {
+            front_anim.setTarget(correctCardImage)
+            back_anim.setTarget(backCardImage)
+            back_anim.start()
+            front_anim.start()
+            true
         }
     }
 }
--- app/src/main/java/com/example/uznair/IncorrectFragment.kt
+++ app/src/main/java/com/example/uznair/IncorrectFragment.kt
@@ -1,5 +1,7 @@
 package com.example.uznair
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -13,9 +15,12 @@ import androidx.fragment.app.Fragment
 class IncorrectFragment : Fragment() {
 
     lateinit var totalScore : TextView
-    lateinit var newCard : TextView
     lateinit var highScoreButton : Button
     lateinit var incorrectCardImage : ImageView
+    lateinit var backCardImage : ImageView
+    lateinit var front_anim : AnimatorSet
+    lateinit var back_anim : AnimatorSet
+    var isFront : Boolean = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,9 +31,9 @@ class IncorrectFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_incorrect, container, false)
         totalScore = view.findViewById(R.id.totalScoreView)
-        newCard = view.findViewById(R.id.newCard)
 
         incorrectCardImage = view.findViewById(R.id.incorrectFragmentCard)
+        backCardImage = view.findViewById(R.id.backIncorrectFragmentCard)
         var cardNumber = (activity as GameActivity).newRandomNumber
 
         showCard(cardNumber)
@@ -36,7 +41,6 @@ class IncorrectFragment : Fragment() {
         var playerScore = (activity as GameActivity).playerScore.toString()
 
         totalScore.text = getString(R.string.total_score, playerScore)
-        newCard.text = (activity as GameActivity).newRandomNumber.toString()
 
         highScoreButton = view.findViewById(R.id.gameOverHighScoreButton)
 
@@ -79,6 +83,27 @@ class IncorrectFragment : Fragment() {
             10 -> {
                 incorrectCardImage.setImageResource(R.drawable.card10)
             }
+        }
+        val context = (activity as GameActivity).applicationContext
+        val scale = context.resources.displayMetrics.density
+        incorrectCardImage.cameraDistance = 8000 * scale
+        backCardImage.cameraDistance = 8000 * scale
+
+        front_anim = AnimatorInflater.loadAnimator(context, R.animator.front_animator) as AnimatorSet
+        back_anim = AnimatorInflater.loadAnimator(context, R.animator.back_animator) as AnimatorSet
+
+        isFront = if(isFront){
+            front_anim.setTarget(backCardImage)
+            back_anim.setTarget(incorrectCardImage)
+            front_anim.start()
+            back_anim.start()
+            false
+        } else {
+            front_anim.setTarget(incorrectCardImage)
+            back_anim.setTarget(backCardImage)
+            back_anim.start()
+            front_anim.start()
+            true
         }
     }
 
